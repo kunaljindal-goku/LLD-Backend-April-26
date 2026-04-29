1e611976ef3bd97347c84e1983f9134cc41453e5
Kunal Jindal <kunaljindal@Kunals-MacBook-Pro.local>
2026-04-29 09:19:13 +0530

    Concurrency Problems

---
+++ Multithreading/Problems/src/UrlScrapping/ScrapURL.java
@@ -0,0 +1,18 @@
+package UrlScrapping;
+
+
+import java.util.concurrent.Callable;
+
+public class ScrapURL implements Callable<Void> {
+    private String url;
+
+    public ScrapURL(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public Void call() throws Exception {
+        Thread.sleep(200);
+        return null;
+    }
+}
